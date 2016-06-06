86a343618c221cbc34f427fadf7298b50fb9e923
Sai Saran Vaidyanathan <saisaranvaidyanathan@ITs-MacBook-Pro-94.local>
2016-06-06 14:36:09 -0700

    Tool to faciliate Apigee Edge Organization maintenance

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# ApigeeBundleReaper
+ApigeeBundleReaper
+===================
+
+Tool to faciliate Apigee Edge Organization maintenance
+
+A  library providing utility to determine the proxies that are not deployed or being used (No Traffic)
+
+A future version will include downloading the proxy bundle to a local directory, delete the bundle from Apigee Edge
+
+Features include:
+* Listing API proxies that are not deployed (for a given environment or all environments)
+* Listing API proxies that has no traffic for a specified number of days (for a given environment or all environments)
+
+## Installation
+
+The only prerequisites not handled during the installation are a functional Node environment, the availability of npm, and sufficient priviledges to run commands as adminstrator. The steps below are applicable to a Mac OS X environment, similar steps work under Linux or Windows. 
+	
+Clone the bundle-reaper repository to your local machine:
+
+	ApigeeCorporation$ git clone https://github.com/ssvaidyanathan/bundle-reaper.git
+
+Alternatively you can download the zip file via the GitHub home page and unzip the archive.
+
+Navigate to the package directory:
+
+	ApigeeCorporation$ cd path/to/bundle-reaper/src/package/
+
+Install npm modules:
+
+	ApigeeCorporation$ sudo npm install
+
+## Usage
+	
+	ApigeeCorporation$ node reaper_env.js
+	prompt: Please provide Org Name:  saisarantest
+	prompt: Please provide Env Name:  (test) test
+	prompt: Please provide number of days:  (90) 90
+	prompt: Please provide Org User ID:  ssvaidyanathan@apigee.com
+	prompt: Please provide Org User Password:
+	
+	Output:
+	-------------------------Undeployed APIs in test-----------------------------
+	oauth10a-3legged
+	emailer
+	authorize
+	json-validate
+	catchAll
+	token_v3_rev3_2016_03_01
+	-----------------------------------------------------------------------------
+	
+	--------------APIs with No Traffic in test in the last 90 days---------------
+	java-cookbook
+	base64encoder
+	jira-release-notes
+	ratingsService
+	oauth10a-3legged
+	response-cache
+	oauth-login-app
+	oauth-client-credentials
+	-----------------------------------------------------------------------------
+
+Similarly for reaper_all.js
+	
+	ApigeeCorporation$ node reaper_all.js
+	prompt: Please provide Org Name:  saisarantest
+	prompt: Please provide number of days:  (90) 90
+	prompt: Please provide Org User ID:  ssvaidyanathan@apigee.com
+	prompt: Please provide Org User Password:
+	
+	Output:
+	-------------------------Undeployed APIs in test-----------------------------
+	oauth10a-3legged
+	emailer
+	authorize
+	json-validate
+	catchAll
+	token_v3_rev3_2016_03_01
+	-----------------------------------------------------------------------------
+	
+	--------------APIs with No Traffic in test in the last 90 days---------------
+	java-cookbook
+	base64encoder
+	jira-release-notes
+	ratingsService
+	oauth10a-3legged
+	response-cache
+	oauth-login-app
+	oauth-client-credentials
+	-----------------------------------------------------------------------------
+
+	-------------------------Undeployed APIs in prod-----------------------------
+	json-validate
+	catchAll
+	token_v3_rev3_2016_03_01
+	-----------------------------------------------------------------------------
+	
+	--------------APIs with No Traffic in prod in the last 90 days---------------
+	java-cookbook
+	base64encoder
+	response-cache
+	oauth-login-app
+	oauth-client-credentials
+	-----------------------------------------------------------------------------
---
+++ src/package/reaper_env.js
@@ -0,0 +1,197 @@
+var prompt = require('prompt');
+var request = require('request');
+var https = require("https");
+var _ = require('lodash');
+
+  var mgmtAPIConfigurations = {
+  	host: 'api.enterprise.apigee.com',
+  	version: 'v1'
+  }
+
+  var properties = [
+    {
+      description: 'Please provide Org Name',
+      name: 'org',
+      required: true,
+      message: 'Must not be empty'
+    },
+    {
+      description: 'Please provide Env Name',
+      name: 'env',
+      required: true,
+      default: 'test',
+      message: 'Must not be empty'
+    },
+    {
+      description: 'Please provide number of days',
+      name: 'axDays',
+      type: 'integer',
+      required: true,
+      default: 90,
+      message: 'Must not be empty and must be a Number'
+    },
+    {
+      description: 'Please provide Org User ID',
+      name: 'orgUserName',
+      required: true,
+      message: 'Must not be empty'
+    },
+    {
+      description: 'Please provide Org User Password',
+      name: 'orgUserPwd',
+      required: true,
+      hidden: true,
+      message: 'Must not be empty'
+    }
+  ];
+
+  prompt.start();
+
+  prompt.get(properties, function (err, result) {
+    if (err) { 
+    	return onErr(err); 
+    } else {
+    	//printInfo(result);
+      getAllAPIs(result);
+    }
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return -1;
+   }
+
+  function printInfo(result) {
+    console.log('Command-line input received:');
+    console.log('  Org: ' + result.org);
+    console.log('  Env: ' + result.env);
+    console.log('  AX Days: ' + result.axDays);
+    console.log('  Username: ' + result.orgUserName);
+    console.log('  Password: ' + result.orgUserPwd);
+    return 1;
+  }
+
+  function getAllAPIs(result){
+  	var data = "";
+
+  	var options = {
+        host: "api.enterprise.apigee.com",
+        port: 443,
+        path: '/'+mgmtAPIConfigurations.version+'/organizations/'+result.org+'/apis',
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: "Basic " + (new Buffer(result.orgUserName + ":" + result.orgUserPwd)).toString("base64")
+        }
+    };
+
+    var req = https.request(options, function(res) {
+        res.on("data", function(d) {
+            data += d;
+        });
+        res.on("end", function() {
+        	//Nothing to do
+          var allApis = JSON.parse(data);
+          getDeployedAPIs(result, allApis);
+        });
+    });
+
+    req.on("error", function(e) {
+        console.error(e);
+    });
+    
+    req.end();
+    
+  }
+
+  function getDeployedAPIs(result, allApis){
+    var data = "";
+
+    var options = {
+        host: "api.enterprise.apigee.com",
+        port: 443,
+        path: '/'+mgmtAPIConfigurations.version+'/organizations/'+result.org+'/environments/'+result.env+'/deployments',
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: "Basic " + (new Buffer(result.orgUserName + ":" + result.orgUserPwd)).toString("base64")
+        }
+    };
+    var req = https.request(options, function(res) {
+        res.on("data", function(d) {
+            data += d;
+        });
+        res.on("end", function() {
+          //console.log(JSON.parse(data).aPIProxy.length);
+          var apiProxies = JSON.parse(data).aPIProxy;
+          var deployedApis = [];
+          for(var i = 0; i<apiProxies.length; i++){
+            deployedApis[i] = apiProxies[i].name;
+          }
+          var undeployedApis = _.difference(allApis, deployedApis);
+          if(undeployedApis!==null && undeployedApis.length > 0){
+            console.log('-------------------------Undeployed APIs in '+result.env+'-----------------------------');
+            undeployedApis.forEach(function(api) {
+              console.log(api);
+            });
+            console.log('-----------------------------------------------------------------------------');
+          }
+          getTraffic(result, allApis);
+        });
+    });
+    req.on("error", function(e) {
+        console.error(e);
+    });
+    req.end();
+  }
+
+  function getTraffic(result, allApis){
+    var toDate = new Date();
+    var formattedToDate = (toDate.getMonth()+1)+'/'+toDate.getDate()+'/'+toDate.getFullYear()+'%2000:00';//MM/DD/YYYY%20HH:MM
+    //console.log(formattedToDate);
+    var fromDate = new Date(toDate - (result.axDays*24*3600*1000));
+    var formattedFromDate = (fromDate.getMonth()+1)+'/'+fromDate.getDate()+'/'+fromDate.getFullYear()+'%2000:00';//MM/DD/YYYY%20HH:MM
+    //  console.log(formattedFromDate);
+    var data = "";
+    var calledApis = [];
+    var options = {
+        host: "api.enterprise.apigee.com",
+        port: 443,
+        path: '/'+mgmtAPIConfigurations.version+'/organizations/'+result.org+'/environments/'+result.env+'/stats/apis?select=sum(message_count)&timeUnit=day&timeRange='+formattedFromDate+'~'+formattedToDate,
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: "Basic " + (new Buffer(result.orgUserName + ":" + result.orgUserPwd)).toString("base64")
+        }
+    };
+
+    var req = https.request(options, function(res) {
+        res.on("data", function(d) {
+            data += d;
+        });
+        res.on("end", function() {
+          var environments = JSON.parse(data).environments;
+          environments.forEach(function(environment) {
+              var dimensions = environment.dimensions;
+              if(dimensions!=null && dimensions.length>0){
+                dimensions.forEach(function(dimension) {
+                  calledApis.push(dimension.name);
+                })
+              }
+            })
+          var unusedApis = _.difference(allApis, calledApis);
+          if(unusedApis!=null && unusedApis.length > 0){
+            console.log('--------------APIs with No Traffic in '+result.env+' in the last '+result.axDays+' days---------------');
+            unusedApis.forEach(function(api) {
+              console.log(api);
+            });
+            console.log('-----------------------------------------------------------------------------');
+          }
+        });
+    });
+    req.on("error", function(e) {
+        console.error(e);
+    });
+    req.end();
+
+  }
