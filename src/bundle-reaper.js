633b67aee6cea344d883ff5a901d1dba41937bc1
Sai Saran Vaidyanathan <saisaranvaidyanathan@ITs-MacBook-Pro-94.local>
2016-06-15 13:23:17 -0700

    Fix Codacy issues

--- src/bundle-reaper.js
+++ src/bundle-reaper.js
@@ -2,43 +2,49 @@ var program = require("commander");
 var colors = require("colors");
 var proxyStatus = require("./package/getProxyStatus");
 
-function make_red(txt) {
+
+function makeRed(txt) {
   return colors.red(txt); //display the help text in red on the console 
 }
 
+
 program
   .version("0.0.1")
-  .option("-h, --host [host]", "Please provide Management API Host Information [api.enterprise.apigee.com]")
-  .option("-o, --organization [organization]", "Please provide the Edge Organization Name")
-  .option("-e, --environment [environment]", "Please provide the Environment name [all | test]")
-  .option("-a, --authorization [authorization]", "Please provide the Edge Basic auth credentials [Basic <auth>]")
-  .option("-d, --axDays [axDays]", "Please provide the number of days for Traffic", parseInt)
+  .option("-h, --host <host>", "Please provide Management API Host Information [api.enterprise.apigee.com]")
+  .option("-o, --organization <organization>", "Please provide the Edge Organization Name")
+  .option("-e, --environment <environment>", "Please provide the Environment name [all | test]")
+  .option("-a, --authorization <authorization>", "Please provide the Edge Basic auth credentials [Basic <auth>]")
+  .option("-d, --axDays <axDays>", "Please provide the number of days for Traffic", parseInt)
   .parse(process.argv);
 
-var flag = true;
+if (!process.argv.slice(2).length) {
+    program.outputHelp(makeRed);
+    process.exit(1);
+ }
 
-if(program.host === undefined || program.host === true){
+var flag = true;
+if(typeof program.host === "undefined" || program.host === true){
 	console.log(colors.red("Please provide Management API Host Information [api.enterprise.apigee.com]"));
 	flag = false;
 }
-if(program.organization === undefined || program.organization === true){
+if(typeof program.organization === "undefined" || program.organization === true){
 	console.log(colors.red("Please provide the Edge Organization Name"));
 	flag = false;
 }
-if(program.environment === undefined || program.environment === true){
+if(typeof program.environment === "undefined" || program.environment === true){
 	console.log(colors.red("Please provide the Environment name [all | test]"));
 	flag = false;
 }
-if(program.authorization === undefined || program.authorization === true){
+if(typeof program.authorization === "undefined" || program.authorization === true){
 	console.log(colors.red("Please provide the Edge Basic auth credentials [Basic <auth>]"));
 	flag = false;
 }
-if(program.axDays === undefined || program.axDays === true){
+if(typeof program.axDays === "undefined" || program.axDays === true){
 	console.log(colors.red("Please provide the number of days for Traffic"));
 	flag = false;
 }
 if(!flag){
-	return;
+	process.exit(1);
 }
 
 proxyStatus.exportAPITrafficStatus({
