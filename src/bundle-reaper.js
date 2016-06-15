617c20617de1a5c49bd6b1c5b205d375bca546ca
Sai Saran Vaidyanathan <saisaranvaidyanathan@ITs-MacBook-Pro-94.local>
2016-06-15 12:41:07 -0700

    Error handling

--- src/bundle-reaper.js
+++ src/bundle-reaper.js
@@ -2,9 +2,13 @@ var program = require("commander");
 var colors = require("colors");
 var proxyStatus = require("./package/getProxyStatus");
 
+function make_red(txt) {
+  return colors.red(txt); //display the help text in red on the console 
+}
+
 program
   .version("0.0.1")
-  .option("-h, --host [host]", "Please provide Management API Host Information")
+  .option("-h, --host [host]", "Please provide Management API Host Information [api.enterprise.apigee.com]")
   .option("-o, --organization [organization]", "Please provide the Edge Organization Name")
   .option("-e, --environment [environment]", "Please provide the Environment name [all | test]")
   .option("-a, --authorization [authorization]", "Please provide the Edge Basic auth credentials [Basic <auth>]")
@@ -12,23 +16,24 @@ program
   .parse(process.argv);
 
 var flag = true;
-if(typeof program.host === undefined || program.host === true){
+
+if(program.host === undefined || program.host === true){
 	console.log(colors.red("Please provide Management API Host Information [api.enterprise.apigee.com]"));
 	flag = false;
 }
-if(typeof program.organization === undefined || program.organization === true){
+if(program.organization === undefined || program.organization === true){
 	console.log(colors.red("Please provide the Edge Organization Name"));
 	flag = false;
 }
-if(typeof program.environment === undefined || program.environment === true){
+if(program.environment === undefined || program.environment === true){
 	console.log(colors.red("Please provide the Environment name [all | test]"));
 	flag = false;
 }
-if(typeof program.authorization === undefined || program.authorization === true){
+if(program.authorization === undefined || program.authorization === true){
 	console.log(colors.red("Please provide the Edge Basic auth credentials [Basic <auth>]"));
 	flag = false;
 }
-if(typeof program.axDays === undefined || program.axDays === true){
+if(program.axDays === undefined || program.axDays === true){
 	console.log(colors.red("Please provide the number of days for Traffic"));
 	flag = false;
 }
